f5a40a00a258a7a5fc0e73c931bddb5ae9dbf340
steventingate <111670868+steventingate@users.noreply.github.com>
2025-06-03 08:34:37 +1000

    Add files via upload

--- utils/db.py
+++ utils/db.py
@@ -2,10 +2,11 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import streamlit as st
+import os
 
 def get_recent_documents(limit=20):
     try:
-        conn = psycopg2.connect(st.secrets["DB_URL"], cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(os.environ.get("DB_URL"), cursor_factory=RealDictCursor)
         cur = conn.cursor()
         cur.execute("SELECT file_name, file_type, uploaded_at FROM documents ORDER BY uploaded_at DESC LIMIT %s", (limit,))
         docs = cur.fetchall()
